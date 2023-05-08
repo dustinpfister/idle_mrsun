3ed128627deffd07ade8a656366f6344506fb5b7
dustinpfister <dustin.pfister@gmail.com>
2023-05-08 10:07:04 -0400

    nodejs and server script started

---
+++ nodejs/server-static/README.md
@@ -0,0 +1,8 @@
+# server-static
+
+Although this is very much an electionjs applaction, I do still want to make a 'browser' version of this game as well of course. So I have this static server script that will host the browser version of the game over a local network. This is mainly for testing reasons on my end, but it is also nice to be able to just host the game on a network also of source
+
+
+## Resources
+
+I started with the source code found [here](https://gist.github.com/anukulSingh/88e20175cf37151773fbbbb194fb460f) then hacked over the code as needed. I have made scripts like this in the past but I think I would like to have a fresh start with this. At the time of this writing I have not added expresss to the stack so as long as that is the case I am going to need some kind of vanilla js soution.
---
+++ nodejs/server-static/index.js
@@ -0,0 +1,58 @@
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+
+const DIR_ROOT = path.join(__dirname, '../../html');
+
+const server = http.createServer( (req, res) => {
+
+    let filePath = path.join(DIR_ROOT, req.url === '/' ? 'index.html' : req.url);
+
+    let extname = path.extname(filePath);
+    let contentType = 'text/html';
+    switch(extname) {
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.mjs':
+            contentType = 'text/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.json':
+            contentType = 'application/json';
+            break;
+        case '.png':
+            contentType = 'image/png';
+            break;
+        case '.jpg':
+            contentType = 'image/jpg';
+            break;
+    }
+    if(contentType == 'text/html' && extname == '') {
+        filePath += '.html'
+    }
+    fs.readFile(filePath, (err, content) => {
+        if (err) {
+            if(err.code == 'ENOENT') {
+                fs.readFile(path.join(__dirname, 'public', '404.html'), (err,content) => {
+                    res.writeHead(404, { 'Content-Type': 'text/html' });
+                    res.end(content, 'utf8');
+                })
+            } else {
+                res.writeHead(500);
+                res.end(`Server error: ${err.code}`);
+            }
+        } else {
+            res.writeHead(200, { 'Content-Type': contentType });
+            res.end(content, 'utf8');
+        }
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => { 
+    console.log(`Server running on Port: ${PORT}`);
+})
